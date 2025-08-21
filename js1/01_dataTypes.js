655c1bf6de8f45507cc7a607a25a95da91fb9a2c
Bittuchand80 <bittuchand80@gmail.com>
2025-08-21 06:04:19 +0000

    DataTypes

---
+++ js1/01_dataTypes.js
@@ -0,0 +1,18 @@
+let name = "Apurba"
+let age = 24
+let isLoggedIn = false
+
+
+// dataTypes - 
+// number => 2 to power 53
+// bigit
+// srting => ""
+// boolean => true/flase (yes or no)
+// null => standloan value. null is a object.
+// undefined => undefined (undefined value is undfined)
+// symbol => unique
+
+
+// object => maharati
+
+console.log(typeof Number,String,Symbol);
