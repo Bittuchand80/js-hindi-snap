6061233623a835d21704c4c7c0932b7e1c554192
Bittuchand80 <bittuchand80@gmail.com>
2025-08-22 05:24:54 +0000

    variables.js

---
+++ js1/variables.js
@@ -0,0 +1,12 @@
+const accountId = 123456
+let accountName = "Apurba"
+accountEmail = "chandapurba95@gmail.com"
+accountPasswprd = "@Bittu12"
+acountCity = "Goa"
+accountAge = "24"
+accountMoney ="Hundred"
+
+console.log(accountId);
+
+console.table([accountId,accountName,accountEmail,accountPasswprd,acountCity,accountAge,accountMoney])
+
