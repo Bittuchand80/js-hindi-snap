9774f8943f95ebd5aa3d46b29343af468536df8b
Bittuchand80 <bittuchand80@gmail.com>
2025-08-27 20:36:00 +0000

    javascript-summary

---
+++ js1/javascript-summary.js
@@ -0,0 +1,57 @@
+// Datatypes
+// Primitive datatypes - string, number, Boolean, null, undefined, symbol, BigInt
+
+const score = 100
+const scoareVlue = 100.5   
+
+const isLoggedIn = false
+
+const outsideTemp = null
+
+let userEmail;
+
+const id = 'abc12'
+const anotherId = '128'
+// console.log(id === anotherId);
+
+const bigNumber = 123577835289n
+
+// Reference(non-primitive datatypes)
+// == Array-[], Object-{}, Functions-(){}
+
+const heros = ["Ironman", "thor", "krishna"]
+
+let myobj = {
+    name: "Bittu",
+    age: 22,
+    number: 8918955240,
+}
+
+ const myFunction = function(){
+     console.log("Hello world");
+}
+
+//    Return type of variables in JavaScript
+// 1) Primitive Datatypes
+    //    Number => number
+    //    String  => string
+    //    Boolean  => boolean
+    //    null  => object
+    //    undefined  =>  undefined
+    //    Symbol  =>  symbol
+    //    BigInt  =>  bigint
+
+// 2) Non-primitive Datatypes
+    //    Arrays  =>  object
+    //   Function  =>  function
+    //   Object  =>  object
+
+
+
+
+
+
+
+
+
+
