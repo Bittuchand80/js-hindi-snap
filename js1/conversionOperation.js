86bed4e562dd2a07cb812cf6a9746aedb134abd8
Bittuchand80 <bittuchand80@gmail.com>
2025-08-27 20:35:23 +0000

    change in location of file

---
+++ js1/conversionOperation.js
@@ -0,0 +1,131 @@
+let score = 33
+// console.log(typeof score);
+// console.log(typeof (score));  ja kono 1ta method
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);   ja kono 1ta method
+// console.log(valueInNumber);
+
+// "33" = 33
+// "33abc" = NaN (Not a Number)
+// true = 1, flase = 0
+
+let pen = Object
+// console.log(typeof null);
+// console.log(typeof undefined);
+
+let someNumber = 722160
+let StringNumber = String(someNumber)
+// console.log(StringNumber);
+// console.log(typeof StringNumber);
+
+let isLoggedIn = 89
+let booleanIsloggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsloggedIn);
+
+// 1 = true, 0 = flase
+// "" = flase
+// "Bittu" = true
+// null = flase
+
+let Name = "Bittu"
+// 
+// console.log("2"+ 1);
+// consoleconsole.log(String);
+// "" = string
+
+let value = 5
+let negValue = value
+// console.log(negValue);
+// console.log(2+2);
+// console.log(2*2);
+// console.log(5-2);
+// console.log(25%5);
+
+let str1 = "khapi"
+let str2 = "khepu"
+let str3 = str1+str2
+// console.log(str3);
+// console.log("1"+ 2);.log("1"+3+2);
+// console.log(2+2+"5");
+// console.log("2"+2+2+1);
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 5
+// console.log(+true);
+// console.log(+"");
+let gameCounter = 101
+++gameCounter;
+// console.log(gameCounter);
+
+let x = 5;
+const y = x++;
+console.log('x:${x}, y:${y}');
+
+let a = 2;
+const b = ++a;
+console.log('a:${a}, b:${b}');
+// console.log("a:{a}, b:{b}");
+// consoleog(a+b);
+// console.log(x+y);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
