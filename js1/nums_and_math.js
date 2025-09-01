ac83a48dfc3f0bc563c0b34b2c1e834e07430c23
Bittuchand80 <bittuchand80@gmail.com>
2025-09-01 05:53:17 +0000

    Number and Maths

---
+++ js1/nums_and_math.js
@@ -0,0 +1,68 @@
+const score = 100
+// console.log(score);
+const balance = new Number(6006)
+// console.log(balance);
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(5));
+
+const anotherNumber = 123.5678
+// console.log(anotherNumber.toPrecision(5));
+
+const twoNumber = 1000000
+// console.log(twoNumber.toLocaleString('en-UK'));
+
+
+// +++++++++++++++++++++ Math +++++++++++++++++++++++++
+
+console.log(Math);
+// console.log(Math.abs(-5));  .abs ar kaj (-) value ke (+) value korbak
+
+// console.log(Math.round(8.8));  .round ar kaj number value take round of kore dibe.
+// example = (4.1) round of 4. (5.9) round of 6.
+
+// console.log(Math.ceil(5.01));
+// ceil mane Top value
+// console.log(Math.floor(5.1));
+// floor mane lowest value
+// console.log(Math.min(6,7,3,9,10)); min = Minimum value
+// console.log(Math.max(5,7,9,10));  max = Maximum value
+
+// console.log(Math.random()); 0 to 1 ar modhe value hobe
+// console.log((Math.random()*10)+1);              ai tar value 1 thake 10 ar modhe hobe
+
+// console.log(Math.floor(Math.random()*10)+1);     ai tar value 1 thke 9 ar modhe hobe
+
+const min = 20
+const max = 40
+console.log(Math.floor(Math.random()*(max-min+1))+min);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
